1a540c544f9d3230de8059f5d001a37e41f84816
Akhil Chirra <akhilchirra.17@gmail.com>
2025-04-17 10:40:26 -0700

    Force update: trigger new Vercel build

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-
-
-
 export default function App() {
   useEffect(() => {
     AOS.init({ duration: 1000 });
