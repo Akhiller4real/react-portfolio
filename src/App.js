bf31b198e2214cc79303db19002dab4f17188537
Akhil Chirra <akhilchirra.17@gmail.com>
2025-04-17 10:01:25 -0700

    Fix: added aos and lock file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
 
 export default function App() {
   useEffect(() => {
